ed3f57ef502488317a800e0c87323da22e6c8efd
pontus.iderbornwinda <pontuswindahl@hotmail.com>
2015-04-30 11:07:06 +0200

    okey

--- start.py
+++ start.py
@@ -11,3 +11,23 @@ with open("bernadotte.yaml") as f:
 
 
 pprint(family)
+
+
+# -*- coding: utf-8 -*-
+#from pprint import pprint
+import yaml
+
+
+with open("bernadotte.yaml") as f:
+    family=yaml.load(f)
+
+    #for name in sorted(f):
+        #print name
+
+
+for name in family['bernadotte']['barn'][0]:
+    print name
+
+
+
+
